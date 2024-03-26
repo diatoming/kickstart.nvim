bd85ba98d4a91e08fb1260d31c83a0a5a2a92977
diatoming <diatoming@gmail.com>
2024-03-26 11:37:37 +0800

    add astro prettier plugin support for conform

--- init.lua
+++ init.lua
@@ -609,6 +609,7 @@ require('lazy').setup({
         -- languages here or re-enable it for the disabled ones.
         local disable_filetypes = { c = true, cpp = true }
         return {
+          async = true,
           timeout_ms = 500,
           lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
         }
@@ -620,7 +621,14 @@ require('lazy').setup({
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
-        -- javascript = { { "prettierd", "prettier" } },
+        -- javascript = { { 'prettierd', 'prettier' } },
+        astro = { 'astro' },
+      },
+      formatters = {
+        astro = {
+          command = 'prettier',
+          args = { '--stdin-filepath', '$FILENAME', '--plugin', 'prettier-plugin-astro' },
+        },
       },
     },
   },
