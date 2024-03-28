1a08d9205497d73e006e65726b77522e0f947918
diatoming <diatoming@gmail.com>
2024-03-28 16:25:44 +0800

    move telescope plugin config from init to custom plugin folder

--- lua/custom/config/keymaps.lua
+++ lua/custom/config/keymaps.lua
@@ -2,4 +2,3 @@ local map = vim.keymap.set
 
 -- ctrl+s to save file
 map({ 'i', 'x', 'n', 's' }, '<C-s>', '<cmd>w<cr><esc>', { desc = 'Save File' })
-map('n', '<leader>ba', '<cmd>wa<cr>', { desc = 'Save all buffers' })
