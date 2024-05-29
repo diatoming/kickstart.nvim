83fc545ea2c7c9150a94b01d09c197476f4f8089
diatoming <diatoming@gmail.com>
2024-05-29 16:44:39 +0800

    add nvim color highlight plugin

--- lua/custom/plugins/ui.lua
+++ lua/custom/plugins/ui.lua
@@ -54,4 +54,15 @@ return {
       }
     end,
   },
+  {
+    'brenoprata10/nvim-highlight-colors',
+    event = 'VeryLazy',
+    config = function()
+      require('nvim-highlight-colors').setup {
+        render = 'background', -- or 'foreground' or 'first_column'
+        enable_named_colors = true,
+        enable_tailwind = false,
+      }
+    end,
+  },
 }
